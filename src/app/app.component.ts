ac54751b99a24cd23dd7d8618cc67754826a622c
Juan Ortiz <jotaatepe@gmail.com>
2023-06-04 18:57:25 -0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChromeExtensionService } from './chrome-extension.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ce-share-local-storage';
+
+  constructor(private readonly ceService: ChromeExtensionService){}
+
+  ngOnInit() {
+    this.ceService.init();
+  }
 }
