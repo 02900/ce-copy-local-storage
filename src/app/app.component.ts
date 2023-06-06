afde9da2304fd7178e4171f98d68e5b892ac214b
02900 <ortiz.juan14@gmail.com>
2023-06-05 21:20:17 -0600

    feat: list open tabs with a substring pipe and tab model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ChromeExtensionService } from './chrome-extension.service';
+import { Tab } from './tab.interface';
 
 @Component({
   selector: 'app-root',
@@ -8,20 +9,36 @@ import { ChromeExtensionService } from './chrome-extension.service';
 })
 export class AppComponent implements OnInit {
   title = 'ce-share-local-storage';
+  tabs: Tab[] = [];
+  sourceTab: Tab | undefined;
+  targetTab: Tab | undefined;
 
-  constructor(private readonly ceService: ChromeExtensionService){}
+  constructor(private readonly ceService: ChromeExtensionService,
+    private readonly cdr: ChangeDetectorRef){}
 
   ngOnInit() {
     this.ceService.init();
-  }
-
-  getToken() {
-    this.ceService.getToken().subscribe((tokens) => {
-      console.log(tokens["olimpo_v2_userCognito"]);
+    this.ceService.getTabs().subscribe((e) => {
+      this.tabs = e;
+      this.cdr.detectChanges();
     });
   }
 
-  setToken() {
+  setSourceTab(tabIndex: string) {
+    this.sourceTab = this.tabs[+tabIndex];
+    this.cdr.detectChanges();
+  }
+
+  setTargetTab(tabIndex: string) {
+    this.targetTab = this.tabs[+tabIndex];
+    this.cdr.detectChanges();
+  }
+
+  sync() {
+    this.ceService.getToken().subscribe((tokens) => {
+      console.log(tokens["olimpo_v2_userCognito"]);
+    });
+
     this.ceService.setToken();
   }
 }
